03966b5ee52e02848f6ea213acfba4d4e00b379c
olesya <nearolesya@gmil.com>
2023-08-10 22:27:57 +0300

    задание3

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine ("Введи любое число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100 || number < 1000)
+{
+   number = number % 10;
+
+    Console.WriteLine(number);
+}
+
+else if (number < 100)
+{
+    Console.WriteLine("Третьей цифры нет"); 
+}
+else (number >= 1000);
+{
+     Console.WriteLine("пока хз"); 
+}
