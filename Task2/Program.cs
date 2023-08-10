3f89dcd14e02e2d8712f0cb02f1705c0021b4cee
olesya <nearolesya@gmil.com>
2023-08-10 21:54:13 +0300

    задание2

---
+++ Task2/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine ("Введи трехзначное число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int number1 = number / 10;
+
+int number2 = number1 % 10;
+
+Console.WriteLine (number2);
